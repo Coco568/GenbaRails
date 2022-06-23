888ebfacc3a546130f20b041d4fc4d1f50330ea9
Coco568 <fmrgse3435@yahoo.co.jp>
2022-06-24 01:15:52 +0900

    if

--- user.rb
+++ user.rb
@@ -13,3 +13,26 @@ class MyClass
         @number
     end
 end
+
+number = 1
+if number == 1
+    puts '整数は1です'
+elsif number == 2
+    puts '整数は2です'    
+else
+    puts '整数は1や2以外です'
+end
+
+number = 100
+message = if number > 50
+    puts "numberは50より大きいです"
+else
+    puts "numberは50以下です"
+end
+
+age = 16
+unless age >= 20
+    puts "未成年者には酒類を提供できません！"
+else
+    puts "おすすめのワインがあります"
+end
