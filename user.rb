74ca4e41049f06ad6fc80f7cf85869e5ead0b6c2
Coco568 <fmrgse3435@yahoo.co.jp>
2022-06-24 00:51:02 +0900

    second-commit

--- user.rb
+++ user.rb
@@ -1,2 +1,15 @@
 class User
+    attr_accessor :name, :address, :email
+    def profile
+        "#{name}(#{address})"
+    end
+end
+
+class MyClass
+    def method_1
+        @number = 100
+    end
+    def method_2
+        @number
+    end
 end
